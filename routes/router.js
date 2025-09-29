3b9e4fa86723b0d8bd0286bace6a5497b69961c8
Atharva884 <atharvalolzzz96@gmail.com>
2025-09-29 17:19:25 +0530

    fix

--- routes/router.js
+++ routes/router.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-//imports here
-const userRoutes = require("./userRoutes.js");
+// //imports here
+// const userRoutes = require("./userRoutes.js");
 
-// //code here
-router.use("/", userRoutes); // All routes (Web + API)
+// // //code here
+// router.use("/", userRoutes); // All routes (Web + API)
 
-router.get("/health-check", (req,res)=>{
-  res.json("Server Health: OK");
-})
+// router.get("/health-check", (req,res)=>{
+//   res.json("Server Health: OK");
+// })
 
-module.exports = router;
+// module.exports = router;
